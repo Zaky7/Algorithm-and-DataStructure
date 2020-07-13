b2af1a78fbe3ac245d44422a18bf5960d17fd709
Zaky7 <zak.saifi7@gmail.com>
2020-07-13 11:01:14 +0530

    Added LinkedList code

---
+++ src/linkedList/Driver.java
@@ -0,0 +1,20 @@
+package linkedList;
+
+import static java.lang.System.out;
+
+class Driver {
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.add(2);
+        ll.displayLL();
+
+        ll.add(3);
+        ll.add(5);
+        ll.add(4);
+
+        ll.displayLL();
+
+        out.println(ll.size());
+    }
+}
--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -1,7 +1,58 @@
-static import System.out;
+package linkedList;
+
+import static java.lang.System.out;
 
 class LinkedList {
-    public static void main(String[] args) {
-        out.println("Hello World");
+
+    private Node head;
+    private Node last;
+    private int _size = 0;
+
+    static class Node   {
+
+        private int value;
+        private Node next;
+
+
+        Node(int val) {
+            this.value = val;
+            this.next = null;
+        }
+
+        public static Node createNewNode(int val) {
+            return new Node(val);
+        }
+
+    }
+
+
+    public void add(int ele) {
+        Node newNode = Node.createNewNode(ele);
+        _size++;
+
+        if(head == null) {
+            head = newNode;
+            last = newNode;
+            return;
+        }
+
+        Node start = last;
+        start.next = newNode;
+        // point last to new Node
+        last = newNode;
+    }
+
+
+    public void displayLL() {
+        Node current = head;
+        while (current != null) {
+            out.print(current.value + " --> ");
+            current = current.next;
+        }
+        out.println();
+    }
+
+    public int size() {
+        return this._size;
     }
 }
